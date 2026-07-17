96de63d73198e399e1a91c688667ab36ef0e1da1
pengcc001-netizen <pengcc001-netizen@users.noreply.github.com>
2026-07-18 02:35:51 +0800

    fix: add BreadcrumbList JSON-LD + mainEntityOfPage to all detail pages

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -34,6 +34,15 @@ export default function BlogPost() {
           "mainEntityOfPage": url,
           "keywords": post.tags.join(', ')
         })}</script>
+        <script type="application/ld+json">{JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement": [
+            { "@type": "ListItem", "position": 1, "name": "Home", "item": "https://rot.csskey.com/" },
+            { "@type": "ListItem", "position": 2, "name": "Blog", "item": "https://rot.csskey.com/blog" },
+            { "@type": "ListItem", "position": 3, "name": post.title, "item": url }
+          ]
+        })}</script>
       </Helmet>
 
       <div style={{ marginBottom: 20 }}>
